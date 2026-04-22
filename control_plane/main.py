d60bdb93382c23b095fdbc897e7599934957265a
Sen-Yao <openclaw@senyao.org>
2026-04-22 19:01:34 +0800

    端口改为8003

--- control_plane/main.py
+++ control_plane/main.py
@@ -414,4 +414,4 @@ async def dashboard_request_detail(request_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
